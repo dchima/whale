c884d1d41f3303184be5b137d7e76511d3ed23e5
dchima <dcnnad@gmail.com>
2021-10-09 09:45:45 +0100

    added queries to the app (to fetch users)

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,10 +4,13 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World!"
+    # end
+
+    field :fetch_users, resolver: Queries::FetchUsers
+    field :fetch_user, resolver: Queries::FetchUser
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  User.create(name: "chima#{i + 1}", email: "chima#{i + 1}@email.com", verified: false, subscriber_status: "unsubscribed")
+end
