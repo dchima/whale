c1e150b398730b5bd457f4a764641df54482a406
dchima <dcnnad@gmail.com>
2021-10-11 01:25:43 +0100

    query to fetch current weather

--- app/graphql/queries/fetch_users.rb
+++ app/graphql/queries/fetch_users.rb
@@ -1,14 +1,9 @@
-require 'weather_api'
 module Queries
   class FetchUsers < Queries::BaseQuery
 
     type [Types::UserType], null: false
 
     def resolve
-      api = WeatherAPI.new()
-      weather = api.getWeather("107.181.180.170")
-      print weather
-
       User.all.order(created_at: :desc)
     end
   end
